30343cb61c0a72faf1c941d47317ef6587b17d6c
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2023-04-02 11:27:54 +1000

    loader, added 3d model to hero section and custom hook for mediaQuery

---
+++ src/components/customHooks/useMediaQuery.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const useMediaQuery = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		// Add a listener for changes to the screen size
+		const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+		// Set the initial value of the 'isMobile' state variable
+		setIsMobile(mediaQuery.matches);
+
+		// Define a callback function to handle changes to the media query
+		const handleMediaQueryChange = (event) => {
+			setIsMobile(event.matches);
+		};
+
+		// Add the callback function as a listener for changes to the media query
+		mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+		// Cleanup function
+		return () => {
+			mediaQuery.removeEventListener('change', handleMediaQueryChange);
+		};
+	}, []);
+
+	return isMobile;
+};
+
+export default useMediaQuery;
