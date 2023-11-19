a2ced4bd2da9e49c2564bbdbcdb1040e318a6c68
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2023-11-19 16:17:08 +1100

    adjust directionalLight and ambientLight to balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,8 +14,8 @@ function Ball(props) {
 
 	return (
 		<Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-			<ambientLight intensity={0.25} />
-			<directionalLight position={[0, 0, 0.05]} />
+			<ambientLight intensity={0.35} />
+			<directionalLight position={[0, 0, 0.15]} />
 			<mesh castShadow receiveShadow scale={2.75}>
 				<icosahedronGeometry args={[1, 1]} />
 				<meshStandardMaterial
