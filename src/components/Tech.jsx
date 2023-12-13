abb6104fb99f05249ddfa983a654d76ba168ddea
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2023-12-14 08:14:01 +1100

    omitting ballcanvas on mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,13 @@
 import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
+import useMediaQuery from './customHooks/useMediaQuery';
 
 function Tech() {
+	const isMobile = useMediaQuery()
 	return (
 		<div className="flex flex-row flex-wrap justify-center gap-10">
-			{technologies.map((technology) => (
+			{isMobile ? null : technologies.map((technology) => (
 				<div className="w-28 h-28" key={technology.name}>
 					<BallCanvas icon={technology.icon} />
 				</div>
