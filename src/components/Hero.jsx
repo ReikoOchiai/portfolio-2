ea49340a972b517de00a468e96b3e85c6351df23
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2023-12-17 11:41:21 +1100

    adding scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,72 @@
-import React from 'react'
+import { useRef, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import { ComputersCanvas } from './canvas'
 import { styles } from '../styles'
-import { useTranslation } from 'react-i18next'
-import useMediaQuery from './customHooks/useMediaQuery'
-import { fadeIn } from '../utils/motion'
+
+import bgImage from '/src/assets/herobg.png'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 function Hero() {
-	const { t } = useTranslation()
-	const isMobile = useMediaQuery()
+	const slider = useRef(null)
+	const firstText = useRef(null)
+	const secondText = useRef(null)
+
+	let xPercent = 0
+	let direction = -1
+
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger)
+		gsap.to(slider.current, {
+			// location of scroll trigger
+			scrollTrigger: {
+				trigger: document.documentElement,
+				scrub: 0.25,
+				start: 0,
+				end: window.innerHeight,
+				onUpdate: (e) => (direction = e.direction * -1),
+			},
+			x: '-=300px',
+		})
+		requestAnimationFrame(animate)
+	}, [])
+
+	const animate = () => {
+		if (xPercent < -100) {
+			xPercent = 0
+		} else if (xPercent > 0) {
+			xPercent = -100
+		}
+		gsap.set(firstText.current, { xPercent: xPercent })
+		gsap.set(secondText.current, { xPercent: xPercent })
+		xPercent += 0.05 * direction
+		requestAnimationFrame(animate)
+	}
 
 	return (
-		<section className="relative w-full h-screen mx-auto">
+		<section className="relative flex overflow-hidden">
 			{' '}
+			<img src={bgImage} alt="Background image" className="object-fit min-h-[100vh]" />
 			<div
 				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl max-auto flex flex-row items-start gap-5`}>
 				<div className="flex flex-col justify-center items-center mt-5">
-					<div className="w-5 h-5 rounded-full bg-[#915eff]" />
 					<div className="w-1 sm:h-80 h-40 violet-gradient" />
 				</div>
 				<div>
-						<h1 className={`${styles.heroHeadText} text-white relative`}>
-							Hi, I'm <span className="text-[#915eff]">Reiko</span>
-						</h1>
+					<h1 className={`${styles.heroHeadText} text-white relative`}>
+						Hi, I'm <span className="text-[#915eff]">Reiko</span>
+					</h1>
 					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
 						Front-end developer and <br className="sm:block hidden" />
 						UI/UX designer 👋
 					</p>
 				</div>
 			</div>
-			{isMobile ? null : <ComputersCanvas />}
+			<div className="sliderContainer">
+				<div ref={slider} className="slider">
+					<p ref={firstText} >Creative Front-end developer -</p>
+					<p ref={secondText}>Creative Front-end developer -</p>
+				</div>
+			</div>
 			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
 				<a href="#about">
 					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
