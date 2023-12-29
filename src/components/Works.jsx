75749ac36050c34b0b40738abe372021988c6335
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2023-12-29 19:08:41 +1100

    changing motion.p to div

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -70,12 +70,12 @@ const Works = () => {
 				<h2 className={styles.sectionHeadText}>Projects.</h2>
 			</motion.div>
 			<div className="w-full flex">
-				<motion.p
+				<motion.div
 					variants={fadeIn('', '', 0.1, 1)}
 					className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
 					Following projects showcases my skills and experience through
 					real-world examples of my work. Currently working on new project in my spare time which cannot wait to share!
-				</motion.p>
+				</motion.div>
 			</div>
 			<div className="mt-20 flex flex-wrap gap-7">
 				{projects.map((project, index) => (
