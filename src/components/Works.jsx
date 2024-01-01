bffc877788d9962bdfe297ede12395b689aad9e7
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2024-01-01 15:49:17 +1100

    add tilt effect back

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,7 +5,7 @@ import { github } from '../assets'
 import { styles } from '../styles'
 import { projects } from '../constants'
 import { SectionWrapper } from '../hoc'
-
+import Tilt from 'react-parallax-tilt'
 const ProjectCard = ({
 	index,
 	name,
@@ -16,46 +16,46 @@ const ProjectCard = ({
 	url,
 }) => {
 	return (
-		<motion.div
-			variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-			className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
-			<div
-				className="relative w-full h-[230px] cursor-pointer"
-				onClick={() => {
-					window.open(url, '_blank')
-				}}>
-				<img
-					src={image}
-					alt={name}
-					className="w-full h-full object-cover rounded-2xl "
-				/>
+		<Tilt>
+			<div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+				<div
+					className="relative w-full h-[230px] cursor-pointer"
+					onClick={() => {
+						window.open(url, '_blank')
+					}}>
+					<img
+						src={image}
+						alt={name}
+						className="w-full h-full object-cover rounded-2xl "
+					/>
 
-				<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-					<button
-						onClick={() => window.open(source_code_link, '_blank')}
-						className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
-						<img
-							src={github}
-							alt="github"
-							className="w-1/2 h-1/2 object-contain"
-						/>
-					</button>
+					<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+						<button
+							onClick={() => window.open(source_code_link, '_blank')}
+							className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+							<img
+								src={github}
+								alt="github"
+								className="w-1/2 h-1/2 object-contain"
+							/>
+						</button>
+					</div>
+				</div>
+
+				<div className="mt-5">
+					<h3 className="text-white font-bold text-[24px]">{name}</h3>
+					<p className="mt-2 text-secondary text-[14px]">{description}</p>
+				</div>
+
+				<div className="mt-4 flex flex-wrap gap-2">
+					{tags.map((tag) => (
+						<p key={tag.name} className={`text-[14px] ${tag.color}`}>
+							#{tag.name}
+						</p>
+					))}
 				</div>
 			</div>
-
-			<div className="mt-5">
-				<h3 className="text-white font-bold text-[24px]">{name}</h3>
-				<p className="mt-2 text-secondary text-[14px]">{description}</p>
-			</div>
-
-			<div className="mt-4 flex flex-wrap gap-2">
-				{tags.map((tag) => (
-					<p key={tag.name} className={`text-[14px] ${tag.color}`}>
-						#{tag.name}
-					</p>
-				))}
-			</div>
-		</motion.div>
+		</Tilt>
 	)
 }
 
@@ -66,11 +66,13 @@ const Works = () => {
 				<p className={styles.sectionSubText}>My work</p>
 				<h2 className={styles.sectionHeadText}>Projects.</h2>
 			</motion.div>
-			<p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-				Following projects showcases my skills and experience through real-world
-				examples of my work. Currently working on new project in my spare time
-				which cannot wait to share!
-			</p>
+			<motion.div variants={fadeIn('', '', 0.1, 1)}>
+				<p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+					Following projects showcases my skills and experience through
+					real-world examples of my work. Currently working on new project in my
+					spare time which cannot wait to share!
+				</p>
+			</motion.div>
 			<div className="mt-20 flex flex-wrap gap-7">
 				{projects.map((project, index) => (
 					<div key={index}>
